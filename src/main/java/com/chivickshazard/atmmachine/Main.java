baa8e7e6c8a6a70096ca98d68c2cdfb2a87b6eea
Victor Chgbo <chigbovcm2020@gmail.com>
2025-09-01 18:32:46 +0100

    chore: initialize project

---
+++ src/main/java/com/chivickshazard/atmmachine/Main.java
@@ -0,0 +1,7 @@
+package com.chivickshazard.atmmachine;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hellnfnofneirfniojfoejfieorfjerfjoerfjerioo world!");
+    }
+}
